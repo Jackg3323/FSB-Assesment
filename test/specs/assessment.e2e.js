321e43c18d44b14da1e380c94e21c60682c427ef
Jack Guthrie <jackg3323@gmail.com>
2022-09-01 21:26:24 +0100

    Add Step 1 and 2

--- test/specs/assessment.e2e.js
+++ test/specs/assessment.e2e.js
@@ -1,5 +1,25 @@
-describe("PlaceHolder", function () {
+describe("FSB Assessment", function () {
   it("Placeholder", async function () {
-    await browser.url("https://www.google.com/").open();
+    // Step 1 - open google
+    await browser.url("https://www.google.com/");
+
+    // reject cookies
+    const cookie = $("div=Reject all");
+    await cookie.click();
+
+    // get what is in title
+    let title = await browser.getTitle();
+
+    // check it includes the text google
+    expect(title).toEqual("Google");
+
+    // Step 2 - find the input box, enter the search term and press enter to run search
+    const gInput = $("input[name='q']");
+    await gInput.setValue("Test Automation Learning");
+    await browser.keys("\uE007");
+
+    // get what is in search and check the search term is included
+    title = await browser.getTitle();
+    expect(browser).toHaveTextContaining("Test+Automation+Learning");
   });
 });
