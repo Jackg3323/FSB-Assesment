92d01e6923c8f22e75a82ee1a48a121dc63553ea
Jack Guthrie <jackg3323@gmail.com>
2022-09-01 22:09:55 +0100

    Added Pauses to Step 5

--- test/specs/assessment.e2e.js
+++ test/specs/assessment.e2e.js
@@ -1,14 +1,14 @@
 describe("FSB Assessment", function () {
   it("Placeholder", async function () {
     // Step 1 - open google
-    await browser.url("https://www.google.com/");
+    browser.url("https://www.google.com/");
 
     // reject cookies
     const cookie = $("div=Reject all");
     await cookie.click();
 
     // get what is in title
-    let title = await browser.getTitle();
+    const title = await browser.getTitle();
 
     // check it includes the text google
     expect(title).toEqual("Google");
@@ -18,16 +18,21 @@ describe("FSB Assessment", function () {
     await gInput.setValue("Test Automation Learning");
     await browser.keys("\uE007");
 
-    // get what is in search and check the search term is included
-    title = await browser.getTitle();
+    // get what is in search url and check the search term is included
     expect(browser).toHaveTextContaining("Test+Automation+Learning");
 
-    // step 3 and 4 - find a link including udemy and click it
+    // step 3 and 4 - find a link including Udemy and click it
     const udemy = $("a*=Udemy");
     await udemy.click();
-    title = await browser.getTitle();
     expect(browser).toHaveTextContaining("udemy");
 
-    //
+    // step 5 - search Udemy for BDD with Cucumber
+    await browser.pause(1000);
+    const uSearch = $("input[name='q']");
+    await uSearch.setValue("BDD with Cucumber");
+    await browser.pause(1000);
+    await browser.keys("\uE007");
+    await browser.pause(1000);
+    expect(browser).toHaveTextContaining("BDD");
   });
 });
